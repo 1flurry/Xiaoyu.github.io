1c8896f1d40fe8cb4e20570e23e83691068b5499
1flurry <33255942+1flurry@users.noreply.github.com>
2018-09-11 17:32:25 +0800

    Update server.py

--- security/server.py
+++ security/server.py
@@ -1,10 +1,10 @@
 import os
 import socket
-import signal
-import asyncio
-import aiohttp
+import hashlib
 import threading
 import time
+import server_file
+import struct
 
 def InitMaxClientNum():
     buf = ""
@@ -33,19 +33,95 @@ def tcplink(sock, addr):
             break
         sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
-    print('Connection from %s closed.' % addr)
+    print('Connection from '+str(addr)+' closed')
 
-class MainThread:
-    def  __init__(self):
-        self.reuse = 1
-        self.i = 0
-        self.new_fd = 0
-        self.tempsocke_fd = 0
+
+    
+
+class FileServer:
+    def __init__(self):
+        self.dataFormat = '8s32s100s100sl'
         #soketlen_t clie_len
+
+    def struct_pack(self):
+        ret = struct.pack(self.dataFormat, self.action.encode(), self.md5sum.encode(), self.clientfilePath.encode(),
+                          self.serverfilePath.encode(), self.size)
+        return ret
+ 
+    def struct_unpack(self, package):
+        self.action, self.md5sum, self.clientfilePath, self.serverfilePath, self.size = struct.unpack(self.dataFormat, package)
+        self.action = self.action.decode().strip('\x00')
+        self.md5sum = self.md5sum.decode().strip('\x00')
+        self.clientfilePath = self.clientfilePath.decode().strip('\x00')
+        self.serverfilePath = self.serverfilePath.decode().strip('\x00')
+
+    def GetMD5(self,filepath):
+        fd = open(filepath,"r")
+        fcont = fd.readlines()
+        fd.close()
+        fmd5 = hashlib.md5(str(fcont).encode("utf-8"))
+        return fmd5.hexdigest()    
+
+
+    def FileSave(self,sock,addr):
+        print ('Accept new connection from {0}'.format(addr))
+        while True:
+            fileinfo_size = struct.calcsize(self.dataFormat)
+            self.buf = sock.recv(fileinfo_size)
+            if self.buf:
+                self.struct_unpack(self.buf)
+                print(self.action)
+                if self.action.startswith("upload"):
+                    if os.path.isdir(self.serverfilePath):
+                        fileName = (os.path.split(self.clientfilePath))[1]
+                        self.serverfilePath = os.path.join(self.serverfilePath, fileName)
+                    filePath,fileName = os.path.split(self.serverfilePath)
+                    if not os.path.exists(filePath):
+                        sock.send(str.encode('dirNotExist'))
+                    else:
+                        sock.send(str.encode('ok'))
+                        recvd_size = 0
+                        file = open(self.serverfilePath, 'wb')
+                        while not recvd_size == self.size:
+                            if self.size - recvd_size > 1024:
+                                rdata = sock.recv(1024)
+                                recvd_size += len(rdata)
+                            else:
+                                rdata = sock.recv(self.size - recvd_size)
+                                recvd_size = self.size
+                            file.write(rdata)
+                        file.close()
+                        output = self.GetMD5(self.serverfilePath)
+                        if output == self.md5sum:
+                            sock.send(str.encode('ok'))
+                        else:
+                            sock.send(str.encode('md5sum error'))
+                elif self.action.startswith("download"):
+                    if os.path.exists(self.serverfilePath):
+                        self.md5sum = self.GetMD5(self.serverfilePath)
+                        self.action = 'ok'
+                        self.size = os.stat(self.serverfilePath).st_size
+                        ret = self.struct_pack()
+                        sock.send(ret)
+                        fo = open(self.serverfilePath, 'rb')
+                        while True:
+                            filedata = fo.read(1024)
+                            if not filedata:
+                                break
+                            sock.send(filedata)
+                        fo.close()
+                    else:
+                        self.action = 'nofile'
+                        ret = self.struct_pack()
+                        sock.send(ret)
+            sock.close()
+            print('Connection from {0} closed.'.format(addr))
+            break
+
 
     def server(self):
         maxclientnum = InitMaxClientNum()
-        server_port = socket.htons(maxclientnum)
+        server_port = 9999
         host = InitHost()
         try:
             socketfd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -55,7 +131,7 @@ class MainThread:
         else:
             socketfd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             try:
-                socketfd.bind((host,9999))
+                socketfd.bind((host,server_port))
             except:
                 print("Sokcet Bind Error!")
             else:
@@ -69,10 +145,12 @@ class MainThread:
                         # 接受一个新连接:
                         sock, addr = socketfd.accept()
                         # 创建新线程来处理TCP连接:
-                        t = threading.Thread(target=tcplink, args=(sock, addr))
+                        t = threading.Thread(target=self.FileSave, args=(sock, addr))
                         t.start()
-'''
+
+
+
+
 if __name__ == '__main__':
-    test = MainThread()
+    test = FileServer()
     test.server()
-    '''
