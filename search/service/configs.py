be6c18fb94667eaa200d7becd0513f348d95edbc
1flurry <33255942+1flurry@users.noreply.github.com>
2018-09-29 17:20:09 +0800

    Create configs.py

---
+++ search/service/configs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 数据存储
+# DATA_REDIS = {'host': '10.250.179.150', 'port': 6379, 'password': 'redis#dev'}
+# DATA_REDIS = {'host': 'localhost', 'port': 6379, 'password': None}
+DATA_MYSQL = {'host': "localhost", 'user': "root", 'password': "123456", 'database': "AIE_Detect_Result"}
+# CFG_REDIS = {'host': '10.250.179.150', 'port': 6379, 'password': 'redis#dev'}
+CFG_REDIS = {'host': 'localhost', 'port': 6380, 'password': None}
+
+ALGORITHM_CFG_TABLE = '__algorithm_cfg_table__'
+ALGORITHM_RST_TABLE = '__algorithm_rst_'
